532707ecaf2f59bc450fefe7e7fb94c0649057cb
Mehdi <powergame_coder2@yahoo.com>
2020-09-27 11:06:36 -0400

    Added seconds, hours, and days for reports. Moved session to session dir

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import time
+import math
 
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -6,7 +7,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram import Client
 import random
 
-app = Client("bot")
+app = Client("session/bot")
 
 
 audience_ids = ['djnotes']
@@ -25,12 +26,20 @@ def randomInspiration():
 async def job():
     if(app.is_initialized):
         for id in audience_ids: 
-            await app.send_message(id, randomInspiration())
-            await app.send_message(id, f"My age: {time.time() - start_time} seconds")
+            lifetime = math.ceil(time.time() - start_time)
+            if lifetime <= 3600:
+                message = f"My age: {math.ceil(lifetime)} seconds"
+            elif  3600 <= lifetime and lifetime <= 86400:
+                message = f"My age: {math.ceil(lifetime / 3600)} hours"
+            else:
+                message = f"My age: {math.ceil(lifetime / 86400)} days"
+            await app.send_message(id, randomInspiration() +  f"\n{message}")
+            # await app.send_message(id, f"My age: {time.time() - start_time} seconds")
 
 
 scheduler = AsyncIOScheduler()
-scheduler.add_job(job, "interval", seconds = 86400)
+scheduler.add_job(job, "interval", seconds = 5)
+# scheduler.add_job(job, "interval", seconds = 86400)
 
 scheduler.start()
 
