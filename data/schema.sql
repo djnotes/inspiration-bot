2c3a37b8e00cd8cee6523885ae8836f092b8de0b
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2022-04-10 18:35:19 -0400

    Minor fixes

--- data/schema.sql
+++ data/schema.sql
@@ -1,11 +1,12 @@
-CREATE TABLE IF NOT EXISTS subscriber(subscriber_id INT PRIMARY KEY, subscriber_name TEXT,
+CREATE TABLE IF NOT EXISTS subscriber(subscriber_id INT PRIMARY KEY,
   creation_timestamp INT DEFAULT UNIX_TIMESTAMP());
 
+
 CREATE TABLE IF NOT EXISTS inspiration(inspiration_id INT AUTO_INCREMENT, 
-subscriber_id INT, -- In case the inspiration is specific to a certain user, this will be non null
-text TEXT, creation_timestamp INT DEFAULT UNIX_TIMESTAMP(),
-PRIMARY KEY(inspiration_id),
-FOREIGN KEY(subscriber_id) REFERENCES subscriber(subscriber_id) ON DELETE CASCADE); -- If a subscriber is deleted, their custom inspirations will be deleted
+  subscriber_id INT, -- In case the inspiration is specific to a certain user, this will be non null
+  text TEXT, creation_timestamp INT DEFAULT UNIX_TIMESTAMP(),
+  PRIMARY KEY(inspiration_id),
+  FOREIGN KEY(subscriber_id) REFERENCES subscriber(subscriber_id) ON DELETE CASCADE); -- If a subscriber is deleted, their custom inspirations will be deleted
 
 
 -- period is a byte representing the period(s), during which the subscriber wants to be inspired. It is calculated by 
@@ -14,4 +15,4 @@ FOREIGN KEY(subscriber_id) REFERENCES subscriber(subscriber_id) ON DELETE CASCAD
 -- So, 1 means they want only morning inspiration, while 6 means they want it all!
 
 CREATE TABLE IF NOT EXISTS schedule(schedule_id INT AUTO_INCREMENT, period BYTE,
-PRIMARY KEY(schedule_id) );
+  PRIMARY KEY(schedule_id) );
